55647f281a170efd79786cabcf63de4ff27c2863
ppeton-expertime <ppeton@expertime.com>
2026-06-19 09:50:43 +0200

    add stars

--- fabric/nb_from_fabric_UI.Notebook/notebook-content.py
+++ fabric/nb_from_fabric_UI.Notebook/notebook-content.py
@@ -13,7 +13,7 @@
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-print("hello from Fabric UI")
+print("*** hello from Fabric UI ***")
 
 # METADATA ********************
 
